5244ae212548a226ed17ad12ff50eba384e546a2
bhnkc-lpha <lpha@bhnkc.edu.hk>
2025-12-09 09:32:21 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import AngleQuiz from './apps/AngleQuiz';
+import CircleTheorems from './apps/CircleTheorems';  // 新增
 
 function App() {
   return (
-    <BrowserRouter basename="/">  {/* 改成根路徑 */}
+    <BrowserRouter basename="/">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/angle-quiz" element={<AngleQuiz />} />
+        <Route path="/circle-theorems" element={<CircleTheorems />} />  {/* 新增 */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
