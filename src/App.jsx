b618848de86666140287aa87528aa5f4e1bb6f8f
bhnkc-lpha <lpha@bhnkc.edu.hk>
2025-12-11 22:39:23 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/angle-quiz" element={<AngleQuiz />} />
         <Route path="/circle-theorems" element={<CircleTheorems />} />
-        <Route path="/circle-theorems" element={<CircleTheorems />} />  {/* 新增 */}
+        <Route path="/identity" element={<CircleTheorems />} />  {/* 新增 */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
