d72af96ddddf91a3a96c9537a05a6d69b43041a1
bhnkc-lpha <lpha@bhnkc.edu.hk>
2025-12-23 15:58:10 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';  // ✅ 改成 pages
 import AngleQuiz from './apps/AngleQuiz';  // ✅ 改成 apps
 import CircleTheorems from './apps/CircleTheorems';  // ✅ 改成 apps
 import IdentityQuiz from './apps/IdentityQuiz';  // ✅ 改成 apps
+import SimultaneousEqQuiz from './apps/SimultaneousEqQuiz';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/angle-quiz" element={<AngleQuiz />} />
         <Route path="/circle-theorems" element={<CircleTheorems />} />
         <Route path="/identity-quiz" element={<IdentityQuiz />} />
+        <Route path="/simultaneous-eq-quiz" element={<SimultaneousEqQuiz />} />
       </Routes>
     </Router>
   );
